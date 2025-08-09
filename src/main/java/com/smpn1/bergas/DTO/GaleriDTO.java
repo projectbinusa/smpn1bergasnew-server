b13c985316ff6ea01d24deec05c0bfb1388071dd
din-ovita <dindanovitaputeriutami29@gmail.com>
2025-08-09 13:23:01 +0700

    galeri crud

---
+++ src/main/java/com/smpn1/bergas/DTO/GaleriDTO.java
@@ -0,0 +1,40 @@
+package com.smpn1.bergas.DTO;
+
+public class GaleriDTO {
+    private Long id_category;
+    private String foto;
+    private String judul;
+    private String deskripsi;
+
+    public Long getId_category() {
+        return id_category;
+    }
+
+    public void setId_category(Long id_category) {
+        this.id_category = id_category;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+}
--- src/main/java/com/smpn1/bergas/service/CategoryGaleryService.java
+++ src/main/java/com/smpn1/bergas/service/CategoryGaleryService.java
@@ -1,6 +1,9 @@
 package com.smpn1.bergas.service;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -8,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.smpn1.bergas.model.CategoryGalery;
+import com.smpn1.bergas.model.Galeri;
 import com.smpn1.bergas.repository.CategoryGaleryRepository;
 import com.smpn1.bergas.repository.GaleriRepository;
 
@@ -39,29 +43,29 @@ public class CategoryGaleryService {
         update.setCategory(categoryGalery.getCategory());
         return categoryGaleryRepository.save(update);
     }
-    // public Map<String, Boolean> delete(Long id) {
-    //     try {
-    //         // Cek apakah ada foto terkait dengan sarana yang akan dihapus
-    //         if (!galeriRepository.findByIdCategory(id).isEmpty()) {
-    //             // Hapus semua entri foto terkait dengan id sarana
-    //             List<Program> programs = galeriRepository.findByIdCategory(id);
-    //             for (Program program : programs){
-    //                 galeriRepository.deleteById(program.getId());
-    //             }
-    //         }
+    public Map<String, Boolean> delete(Long id) {
+        try {
+            // Cek apakah ada foto terkait dengan sarana yang akan dihapus
+            if (!galeriRepository.findByIdCategory(id).isEmpty()) {
+                // Hapus semua entri foto terkait dengan id sarana
+                List<Galeri> galeries = galeriRepository.findByIdCategory(id);
+                for (Galeri galeri : galeries){
+                    galeriRepository.deleteById(galeri.getId());
+                }
+            }
 
-    //         // Hapus entri sarana setelah semua foto terkait dihapus
-    //         categoryGaleryRepository.deleteById(id);
+            // Hapus entri sarana setelah semua foto terkait dihapus
+            categoryGaleryRepository.deleteById(id);
 
-    //         // Return response berhasil
-    //         Map<String, Boolean> response = new HashMap<>();
-    //         response.put("Deleted", Boolean.TRUE);
-    //         return response;
+            // Return response berhasil
+            Map<String, Boolean> response = new HashMap<>();
+            response.put("Deleted", Boolean.TRUE);
+            return response;
 
-    //     } catch (Exception e) {
-    //         // Return response gagal jika ada error
-    //         return Collections.singletonMap("Deleted", Boolean.TRUE);
-    //     }
-    // }
+        } catch (Exception e) {
+            // Return response gagal jika ada error
+            return Collections.singletonMap("Deleted", Boolean.TRUE);
+        }
+    }
 
 }
